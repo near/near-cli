4ac895d904a09327fbe52a65a9308c3e02b1b110
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-05-23 13:56:16 -0500

    CI: add a test case for create account

--- test/new_project.sh
+++ test/new_project.sh
@@ -8,3 +8,8 @@ npm install
 npm uninstall near-shell
 npm install ../
 NODE_ENV=development npm run test
+timestamp=$(date +%s)
+testaccount=testaccount$timestamp
+../bin/near create_account --account_id $testaccount
+#//../bin/near deploy --contract_name $testaccount
+
