6d54ba69d57e61bf55477ff8f62e774659022760
Vladimir Grichina <vgrichina@gmail.com>
2019-06-07 20:10:17 -0700

    Fix tests paths

--- test/new_project.sh
+++ test/new_project.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 set -ex
 
-cd test/
-
 # remove temporary blank project
 rm  -rf tmp-project
 
 # test generating new project in cwd
 mkdir tmp-project
 cd tmp-project
-../../bin/near new_project
+../bin/near new_project
 #yarn
 #yarn remove near-shell
 #yarn add ../../
@@ -18,7 +16,7 @@ cd ..
 rm  -rf tmp-project
 
 # test generating new project in new dir
-../bin/near new_project 'tmp-project'
+./bin/near new_project 'tmp-project'
 cd tmp-project
 FILE=package.json
 if test -f "$FILE"; then
