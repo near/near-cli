a3d7a5ff0a72fa405a8a1b5a834f4b276aa2b9eb
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-07-31 14:39:20 -0700

    Upgrade blank project to gulp 4 so that it works with latest node

--- blank_project/gulpfile.js
+++ blank_project/gulpfile.js
@@ -1,30 +1,20 @@
 const gulp = require("gulp");
 const nearUtils = require("near-shell/gulp-utils");
 
-gulp.task("build:model", callback => {
-  nearUtils.generateBindings("model.ts", "../out/model.near.ts", callback);
-});
 
-gulp.task("build:bindings", ["build:model"], callback => {
-  nearUtils.generateBindings("main.ts", "../out/main.near.ts", callback);
-});
+function build_bindings(done){
+  nearUtils.generateBindings("main.ts", "../out/main.near.ts", done);
+}
 
-gulp.task("build", ["build:bindings"], callback => {
-  nearUtils.compile("../out/main.near.ts", "../out/main.wasm", callback);
-});
+function build_model(done){
+  nearUtils.generateBindings("model.ts", "../out/model.near.ts", done);
+}
 
-gulp.task("default", ["build"]);
+function build_wasm(done){
+  nearUtils.compile("../out/main.near.ts", "../out/main.wasm", done);
+};
 
-// TODO: Extract all following boilerplate into library
+const build = gulp.series(build_model, build_bindings, build_wasm);
 
-// This task is not required when running the project locally. Its purpose is to set up the
-// AssemblyScript compiler when a new project has been loaded in WebAssembly Studio.
-gulp.task("project:load", () => {
-  const utils = require("@wasm/studio-utils");
-  utils.eval(utils.project.getFile("setup.js").getData(), {
-    logLn,
-    project,
-    monaco,
-    fileTypeForExtension,
-  });
-});
+
+exports.default = build;
