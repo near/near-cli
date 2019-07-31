115c724e111281c6405e855254c6b3988d3bcc68
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-07-31 15:19:29 -0700

    Return default blank project config if there is no config in expected dir

--- get-config.js
+++ get-config.js
@@ -7,8 +7,9 @@ module.exports = function getConfig() {
         return config;
     } catch (e) {
         if (e.code == 'MODULE_NOT_FOUND') {
-            console.warn(`[WARNING] Didn't find config at ${configPath}\n`);
-            return {};
+            console.warn(`[WARNING] Didn't find config at ${configPath}\n, using default shell config`);
+            const defaultConfig = require('./blank_project/src/config')(process.env.NODE_ENV || 'development');
+            return defaultConfig;
         }
         throw e;
     }
