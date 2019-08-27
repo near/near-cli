a0acd670c99cb8cd2e7590f6236bd26f26f491bc
Jane <jane.degtiareva@gmail.com>
2019-08-27 16:41:57 -0700

    Update main.ts in blank project so that it compiles with latest runtime

--- blank_project/assembly/main.ts
+++ blank_project/assembly/main.ts
@@ -26,7 +26,7 @@ export function sayHi(): void {
 
 // This is our view method. It returns the last account_id of a sender who called `sayHi`.
 // It reads value from the persistent store under the key "last_sender" and returns it.
-export function whoSaidHi(): string {
+export function whoSaidHi(): string | null {
   // getString returns a string value for a given string key.
   return storage.getString(LAST_SENDER_KEY);
 }
