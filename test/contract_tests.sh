906b8d9f69f9c673e67db313826f905b4fbdc202
Jane <jane.degtiareva@gmail.com>
2019-08-30 13:08:26 -0700

    Direct call tests

---
+++ test/contract_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+cd tmp-project
+rm  -rf assembly
+mkdir assembly
+timestamp=$(date +%s)
+testaccount=testaccount$timestamp
+../bin/near create_account $testaccount
+echo Building contract
+cp ../node_modules/near-runtime-ts/tests/assembly/*.ts assembly/
+yarn
+yarn build
+echo Deploying contract
+../bin/near deploy --accountId=$testaccount --wasmFile=out/main.wasm
+echo Calling functions
+RESULT=$(../bin/near call $testaccount hello "{}" --accountId=test.near)
+if [[ $RESULT != *"Result: helloa"* ]]; then
+  exit 1
+fi
