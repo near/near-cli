5455dee38157d27aba0e085a53e3f41c7e8138af
Jane <jane.degtiareva@gmail.com>
2019-09-09 14:02:44 -0700

    Remove confusing "failed to load validator key" warning.
    This happens because we try to load the validator key for all environments

--- index.js
+++ index.js
@@ -41,15 +41,11 @@ exports.clean = async function() {
 };
 
 async function connect(options) {
-    if (options.keyPath === undefined && options.helperUrl === undefined) {
-        const homeDir = options.homeDir || `${process.env.HOME}/.near`;
-        options.keyPath = `${homeDir}/validator_key.json`;
-    }
-    // TODO: search for key store.
     const keyStore = new UnencryptedFileSystemKeyStore('./neardev');
     options.deps = {
         keyStore,
     };
+    // TODO: search for key store.
     return await nearjs.connect(options);
 }
 
