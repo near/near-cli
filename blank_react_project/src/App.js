9567d42233b0c3a8f73350b5c7040d4e789fe8dc
Yifang Ma <yifang@nearprotocol.com>
2019-09-24 11:51:00 -0700

    change style for template

--- blank_react_project/src/App.js
+++ blank_react_project/src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
   render() {
     let style = {
       fontSize: "1.5rem",
-      color: "#61dafb",
+      color: "#0072CE",
       textShadow: "1px 1px #D3D3D3"
     }
     return (
