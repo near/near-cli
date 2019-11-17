4c02928aa78afa5c7b5db8d00d1c0ed381657b9f
Sherif Abushadi <sherif@amgando.org>
2019-11-17 13:22:09 +0300

    remove duplicate command

--- bin/near-cli.js
+++ bin/near-cli.js
@@ -2,7 +2,7 @@ const yargs = require('yargs');
 const main = require('../');
 const exitOnError = require('../utils/exit-on-error');
 
-// For account: 
+// For account:
 const createAccount = {
     command: 'create_account <accountId>',
     desc: 'create a new developer account',
@@ -211,7 +211,7 @@ yargs // eslint-disable-line
     .command(deploy)
     .command(scheduleFunctionCall)
     .command(callViewFunction)
-    .command(viewAccount)
+    // .command(viewAccount)
     .command(sendMoney)
     .command(clean)
     .command(stake)
@@ -230,4 +230,3 @@ yargs // eslint-disable-line
     .showHelpOnFail(true)
     .demandCommand(1, 'Please enter a command')
     .argv;
-
