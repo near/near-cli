602e5538d361dbcfaa6e8583c42e16d5c817a54e
Vladimir Grichina <vgrichina@gmail.com>
2019-12-29 22:17:46 -0800

    Fix bug with `near call` passing --amount as gas

--- commands/call.js
+++ commands/call.js
@@ -8,23 +8,29 @@ module.exports = {
     command: 'call <contractName> <methodName> [args]',
     desc: 'schedule smart contract call which can modify state',
     builder: (yargs) => yargs
+        .option('gas', {
+            desc: 'Max amount of gas this call can use',
+            type: 'string',
+            default: '100000000000000'
+        })
         .option('amount', {
             desc: 'Number of tokens to attach',
             type: 'string',
-            default: '0.0000000001'
+            default: '0'
         }),
     handler: exitOnError(scheduleFunctionCall)
 };
 
 async function scheduleFunctionCall(options) {
     console.log(`Scheduling a call: ${options.contractName}.${options.methodName}(${options.args || ''})` +
-        (options.amount ? ` with attached ${utils.format.parseNearAmount(options.amount)} NEAR` : ''));
+        (options.amount && options.amount != '0' ? ` with attached ${utils.format.parseNearAmount(options.amount)} NEAR` : ''));
     const near = await connect(options);
     const account = await near.account(options.accountId);
     const functionCallResponse = await account.functionCall(
         options.contractName,
         options.methodName,
         JSON.parse(options.args || '{}'),
+        options.gas,
         utils.format.parseNearAmount(options.amount));
     const result = nearlib.providers.getTransactionLastResult(functionCallResponse);
     console.log(inspectResponse(result));
