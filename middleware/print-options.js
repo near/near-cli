f9d30c30949e5fa32b6f5fa15bdbde145c75447c
Vladimir Grichina <vgrichina@gmail.com>
2020-02-27 22:16:34 -0800

    Add middleware/print-options.js

---
+++ middleware/print-options.js
@@ -0,0 +1,7 @@
+
+module.exports = async function printOptions(options) {
+    const filteredOptions = Object.keys(options)
+        .filter(key => !key.match(/[-_\$]/))
+        .reduce((obj, key) => ({...obj, [key]: options[key]}), {});
+    console.log('Using options:', filteredOptions);
+}
