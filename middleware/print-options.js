36851cf2ac723aae60474d7376399608c08f051e
Mike Purvis <mikedotexe@gmail.com>
2020-02-28 16:13:02 -0800

    fix linting

--- middleware/print-options.js
+++ middleware/print-options.js
@@ -1,7 +1,7 @@
 
 module.exports = async function printOptions(options) {
     const filteredOptions = Object.keys(options)
-        .filter(key => !key.match(/[-_\$]/))
+        .filter(key => !key.match(/[-_$]/))
         .reduce((obj, key) => ({...obj, [key]: options[key]}), {});
     console.log('Using options:', filteredOptions);
-}
+};
