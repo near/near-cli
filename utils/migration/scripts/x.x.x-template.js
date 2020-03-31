34a6d5caeb7183c591e96f337f7c7a318ad82e7e
Mike Purvis <mikedotexe@gmail.com>
2020-03-31 07:59:36 -0700

    indentation fix

--- utils/migration/scripts/x.x.x-template.js
+++ utils/migration/scripts/x.x.x-template.js
@@ -12,13 +12,13 @@ const upgrade = async (lastPatchVersion) => {
     console.log(`The user last interacted with this project with near-shell version x.x.${lastPatchVersion}`);
 
     /*
-  if (lastPatchVersion < 1) {
-    // implement essential logic that changed from x.x.0 to x.x.1
-  }
+      if (lastPatchVersion < 1) {
+        // implement essential logic that changed from x.x.0 to x.x.1
+      }
 
-  if (lastPatchVersion < 6) {
-    // implement essential logic that changed from x.x.1 to x.x.6
-  }
+      if (lastPatchVersion < 6) {
+        // implement essential logic that changed from x.x.1 to x.x.6
+      }
    */
 };
 
