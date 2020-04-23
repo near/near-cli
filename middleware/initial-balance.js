33d6e0d6fe3886947e36387129801c3fe9b3a782
Vladimir Grichina <vgrichina@gmail.com>
2020-04-22 17:55:31 -0700

    yarn fix

--- middleware/initial-balance.js
+++ middleware/initial-balance.js
@@ -3,4 +3,4 @@ const { utils } = require('near-api-js');
 
 module.exports = async function parseInitialBalance(options) {
     options.initialBalance = utils.format.parseNearAmount(options.initialBalance);
-}
+};
