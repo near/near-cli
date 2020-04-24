1d26261ff1a2d3dca844a645b2480d896d9a4a56
Illia Polosukhin <ilblackdragon@gmail.com>
2020-04-24 14:39:43 -0700

    Keep working on validators display

--- commands/validators.js
+++ commands/validators.js
@@ -3,7 +3,14 @@ const connect = require('../utils/connect');
 const inspectResponse = require('../utils/inspect-response');
 const bs58 = require('bs58');
 const { utils } = require('near-api-js');
+const BN = require('bn.js');
 const AsciiTable = require('ascii-table');
+
+function findSeatPrice(validators) {
+    let stakes = validators.map((v) => new BN(v.stake, 10)).sort();
+    // TODO: find the correct price.
+    return stakes.reduce((a, b) => a.add(b)).div(new BN(stakes.length));
+}
 
 module.exports = {
     command: 'EXPERIMENTAL_validators',
@@ -12,11 +19,12 @@ module.exports = {
         const near = await connect(argv);
 
         const validators = await near.connection.provider.sendJsonRpc('validators', [null]);
-        console.log(`Validators (total: ${validators.current_validators.length}):`);
 
         var validatorsTable = new AsciiTable();
+        let seatPrice = findSeatPrice(validators.current_validators);
+        console.log(`Validators (total: ${validators.current_validators.length}, seat price: ${utils.format.formatNearAmount(seatPrice, 0)}):`);
         validators.current_validators.forEach((validator) => {
-            validatorsTable.addRow(validator.account_id, utils.format.formatNearAmount(validator.stake, 0));
+            validatorsTable.addRow(validator.account_id, utils.format.formatNearAmount(validator.stake, 0), new BN(validator.stake).divRound(seatPrice));
         });
         console.log(validatorsTable.toString());
 
