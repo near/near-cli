c26e156f050dcea21134c866cf9eac03fe6181b7
Jane Degtiareva <jane.degtiareva@gmail.com>
2020-06-05 10:44:59 -0700

    Fix lint errors

--- utils/crash-error-report.js
+++ utils/crash-error-report.js
@@ -1,9 +1,9 @@
 const eventtracking = require('./eventtracking');
 
 const mixPanelProperties = {
-  near_shell_command: process.env.NEAR_CLI_ERROR_LAST_COMMAND,
-  error_message: process.env.NEAR_CLI_LAST_ERROR,
-  network_id: process.env.NEAR_CLI_NETWORK_ID
+    near_shell_command: process.env.NEAR_CLI_ERROR_LAST_COMMAND,
+    error_message: process.env.NEAR_CLI_LAST_ERROR,
+    network_id: process.env.NEAR_CLI_NETWORK_ID
 };
 Object.assign(mixPanelProperties, {});
 eventtracking.track(eventtracking.EVENT_ID_ERROR, mixPanelProperties);
