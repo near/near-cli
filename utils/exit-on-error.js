09f8ed355e1f1232bb9756b63bb6b01f3455c034
janedegtiareva <jane.degtiareva@gmail.com>
2020-06-08 15:06:47 -0700

    Add basic crash reporting to near-cli (#401)
    * Add basic crash reporting to near-cli
    
    This requires adding a workaround to spawn a child process, since
    mixpanel does not handle this case properly.
    
    There is an existing limitation that if a user did not opt in to
    tracking, the crash report will not be sent. We may want to ask an
    additional qustion for this case, since there are scenarios where people
    are willing to send an individual crash report but not opt in to general
    tracking.
    
    There is a known limitation that this implementation does not log the
    environment that was used.
    
    * Add info on network_id to crash reports
    
    * nit: clean up TODOs that were already done
    
    * Fix lint errors
    
    * Remove unused code

--- utils/exit-on-error.js
+++ utils/exit-on-error.js
@@ -1,8 +1,24 @@
+// This is a workaround to get Mixpanel to log a crash
+process.on('exit', () => {
+    require('child_process').fork(__dirname + '/crash-error-report.js', ['node'], {
+        silent: true,
+        detached: true,
+        env: {
+            NEAR_CLI_ERROR_LAST_COMMAND: process.env.NEAR_CLI_ERROR_LAST_COMMAND,
+            NEAR_CLI_LAST_ERROR: process.env.NEAR_CLI_LAST_ERROR,
+            NEAR_CLI_NETWORK_ID: process.env.NEAR_CLI_NETWORK_ID
+        }
+    });
+});
+
 module.exports = (promiseFn) => async (...args) => {
+    process.env.NEAR_CLI_ERROR_LAST_COMMAND = args[0]['_'];
+    process.env.NEAR_CLI_NETWORK_ID = require('../get-config')()['networkId'];
     const promise = promiseFn.apply(null, args);
     try {
         await promise;
     } catch (e) {
+        process.env.NEAR_CLI_LAST_ERROR = e.message;
         console.log('Error: ', e);
         process.exit(1);
     }
