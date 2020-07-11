a1b1a555e6968b83348c88012720217abc471100
Mike Purvis <mikedotexe@gmail.com>
2020-07-10 17:15:25 -0700

    remove default from initFunction

--- bin/near-cli.js
+++ bin/near-cli.js
@@ -104,8 +104,7 @@ const deploy = {
         })
         .option('initFunction', {
             desc: 'Initialization method',
-            type: 'string',
-            default: 'new'
+            type: 'string'
         })
         .option('initArgs', {
             desc: 'Initialization arguments',
