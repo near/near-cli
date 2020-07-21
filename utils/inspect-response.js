9b19f632d84e781866408c9e2619e8653d8ba32a
Jane Degtiareva <jane.degtiareva@gmail.com>
2020-07-21 15:29:50 -0700

    Use transaction id from error to print out explorer link for errors

--- utils/inspect-response.js
+++ utils/inspect-response.js
@@ -10,10 +10,33 @@ const prettyPrintResponse = (response, options) => {
     explorer.printTransactionUrl(txnId, options);
 };
 
+const prettyPrintError = (error, options) => {
+    console.log(`An error occured`)
+    console.log(formatResponse(error));
+    const txnId = getTxnIdFromError(error);
+    if (txnId) {
+        console.log(`We attempted to send a transaction with id ${txnId} to NEAR, but something went wrong.`);
+        explorer.printTransactionUrl(txnId, options);
+    }
+};
+
 const formatResponse = (response) => {
     return util.inspect(response, { showHidden: true, depth: null, colors: true, maxArrayLength: null });
-
 };
+
+const getTxnIdFromError = (error) => {
+    // Currently supported error format: 
+    // TypedError: Exceeded 10 status check attempts for transaction GRPWi935e8Cm2PmPqrcrAyBuM9N13h5kHJNqhfVcY33q.
+    // at Account.retryTxResult (C:\Users\janed\near\near-api-js\lib\account.js:97:15)
+    // at Account.signAndSendTransaction (C:\Users\janed\near\near-api-js\lib\account.js:119:22)
+    // at Object. (C:\Users\janed\near\near-api-js\test\account.test.js:38:5) {
+    // type: 'RetriesExceeded',
+    // transactionHash: 'GRPWi935e8Cm2PmPqrcrAyBuM9N13h5kHJNqhfVcY33q'
+    // } 
+    
+    if (!error) return null;
+    return error.transactionHash;
+}
 
 const getTxnId = (response) => {
     // Currently supported response format: 
@@ -31,6 +54,7 @@ const getTxnId = (response) => {
 
 module.exports = {
     prettyPrintResponse,
+    prettyPrintError,
     formatResponse,
     getTxnId,
 };
