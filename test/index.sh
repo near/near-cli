ff336fbf9d52335d6b6e22856f3d1b4b8ec5b6ec
luciotato <luciotato@gmail.com>
2020-08-23 20:59:11 -0300

    fix: check NODE_ENV, and green/red success/fail

--- test/index.sh
+++ test/index.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export NODE_ENV=${NODE_ENV:-test}
+if [ "$NODE_ENV" == "betanet" -o "$NODE_ENV" == "development" -o "$NODE_ENV" == "testnet" ]; then
+    echo NODE_ENV=$NODE_ENV
+    echo -e "\e[31mFAIL\e[0m tests won't run if NODE_ENV=betanet|development|testnet"
+    echo please export NODE_ENV=test before testing
+    exit 1
+fi
 OVERALL_RESULT=0
 mkdir ~/.near-config
 echo '{"trackingEnabled":false}' >  ~/.near-config/settings.json
@@ -8,10 +14,10 @@ for test in ./test/test_*; do
     echo "Running $test"
     "$test"
     if [ $? -ne 0 ]; then
-        echo "$test FAIL"
+        echo -e "\e[31mFAIL\e[0m $test"
         OVERALL_RESULT=1
     else
-        echo "$test SUCCESS"
+        echo -e "\e[32mSUCCESS\e[0m $test"
     fi
 done
 
