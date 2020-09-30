944d3c65d5644f32a28eaf6c9170ee86307da34b
Yifang Ma <yifang@nearprotocol.com>
2020-09-30 13:58:22 -0700

    build person profile

--- utils/eventtracking.js
+++ utils/eventtracking.js
@@ -1,15 +1,15 @@
-const MIXPANEL_TOKEN = 'e98aa9d6d259d9d78f20cb05cb54f5cb';
+const MIXPANEL_TOKEN = "e98aa9d6d259d9d78f20cb05cb54f5cb";
 
-const chalk = require('chalk');  // colorize output
-const crypto = require('crypto');
-const mixpanel = require('mixpanel').init(MIXPANEL_TOKEN);
-const near_cli_version = require('../package.json').version;
-const readline = require('readline');
-const settings = require('./settings');
-const uuid = require('uuid');
+const chalk = require("chalk"); // colorize output
+const crypto = require("crypto");
+const mixpanel = require("mixpanel").init(MIXPANEL_TOKEN);
+const near_cli_version = require("../package.json").version;
+const readline = require("readline");
+const settings = require("./settings");
+const uuid = require("uuid");
 
-const TRACKING_ENABLED_KEY = 'trackingEnabled';
-const TRACKING_SESSION_ID_KEY = 'trackingSessionId';
+const TRACKING_ENABLED_KEY = "trackingEnabled";
+const TRACKING_SESSION_ID_KEY = "trackingSessionId";
 
 const isGitPod = () => {
     return !!process.env.GITPOD_WORKSPACE_URL;
@@ -19,7 +19,11 @@ const getGitPodUserHash = () => {
     if (!process.env.GITPOD_GIT_USER_EMAIL) {
         return null;
     }
-    return crypto.createHash('sha256').update(process.env.GITPOD_GIT_USER_EMAIL, 'utf8').digest('hex').toString();
+    return crypto
+        .createHash("sha256")
+        .update(process.env.GITPOD_GIT_USER_EMAIL, "utf8")
+        .digest("hex")
+        .toString();
 };
 
 const shouldOptInByDefault = () => {
@@ -30,7 +34,10 @@ const shouldTrack = (shellSettings) => {
     if (shouldOptInByDefault()) {
         return true;
     }
-    return TRACKING_ENABLED_KEY in shellSettings && shellSettings[TRACKING_ENABLED_KEY];
+    return (
+        TRACKING_ENABLED_KEY in shellSettings &&
+        shellSettings[TRACKING_ENABLED_KEY]
+    );
 };
 
 const track = async (eventType, eventProperties, options) => {
@@ -40,41 +47,53 @@ const track = async (eventType, eventProperties, options) => {
     }
     try {
         const mixPanelProperties = {
-            distinct_id: isGitPod() ? getGitPodUserHash() : shellSettings[TRACKING_SESSION_ID_KEY],
+            distinct_id: isGitPod()
+                ? getGitPodUserHash()
+                : shellSettings[TRACKING_SESSION_ID_KEY],
             near_cli_version,
             os: process.platform,
             network_id: options.networkId,
             node_url: options.nodeUrl,
             wallet_url: options.walletUrl,
-            is_gitpod: isGitPod()
+            is_gitpod: isGitPod(),
         };
         Object.assign(mixPanelProperties, eventProperties);
         await mixpanel.track(eventType, mixPanelProperties);
     } catch (e) {
-        console.log('Warning: problem while sending developer event tracking data. This is not critical. Error: ', e);
+        console.log(
+            "Warning: problem while sending developer event tracking data. This is not critical. Error: ",
+            e
+        );
     }
 };
 
 const getEventTrackingConsent = async () => {
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
     });
     try {
         for (let attempts = 0; attempts < 10; attempts++) {
             const answer = await new Promise((resolve) => {
                 rl.question(
                     chalk`We would like to collect data on near-cli usage to improve developer experience.` +
-                    chalk` We will never send private information. We only collect which commands are run via an anonymous identifier.` +
-                    chalk`{bold.yellow  Would you like to opt in (y/n)? }`,
+                        chalk` We will never send private information. We only collect which commands are run via an anonymous identifier.` +
+                        chalk`{bold.yellow  Would you like to opt in (y/n)? }`,
                     async (consentToEventTracking) => {
-                        if (consentToEventTracking == 'y' || consentToEventTracking == 'Y') {
+                        if (
+                            consentToEventTracking == "y" ||
+                            consentToEventTracking == "Y"
+                        ) {
                             resolve(true);
-                        } else if (consentToEventTracking == 'n' || consentToEventTracking == 'N') {
+                        } else if (
+                            consentToEventTracking == "n" ||
+                            consentToEventTracking == "N"
+                        ) {
                             resolve(false);
                         }
                         resolve(undefined);
-                    });
+                    }
+                );
             });
             if (answer !== undefined) {
                 return answer;
@@ -90,11 +109,23 @@ const askForConsentIfNeeded = async (options) => {
     const shellSettings = settings.getShellSettings();
     // if the appropriate option is not in settings, ask now and save settings.
     if (!(TRACKING_ENABLED_KEY in shellSettings)) {
-        shellSettings[TRACKING_ENABLED_KEY] = shouldOptInByDefault() || await getEventTrackingConsent();
-        shellSettings[TRACKING_SESSION_ID_KEY] = shellSettings[TRACKING_ENABLED_KEY] ? uuid.v4() : undefined;
+        shellSettings[TRACKING_ENABLED_KEY] =
+            shouldOptInByDefault() || (await getEventTrackingConsent());
+        shellSettings[TRACKING_SESSION_ID_KEY] = shellSettings[
+            TRACKING_ENABLED_KEY
+        ]
+            ? uuid.v4()
+            : undefined;
         settings.saveShellSettings(shellSettings);
         if (shellSettings[TRACKING_ENABLED_KEY]) {
-            track(module.exports.EVENT_ID_TRACKING_OPT_IN, {}, options);
+            await track(module.exports.EVENT_ID_TRACKING_OPT_IN, {}, options);
+            await mixpanel.people.set({
+                distinct_id: isGitPod()
+                    ? getGitPodUserHash()
+                    : shellSettings[TRACKING_SESSION_ID_KEY],
+                network_id: options.networkId,
+                node_url: options.nodeUrl,
+            });
         }
     }
 };
@@ -104,8 +135,8 @@ module.exports = {
     askForConsentIfNeeded,
     // Some of the event ids are auto-generated runtime with the naming convention event_id_shell_{command}_start
 
-    EVENT_ID_CREATE_ACCOUNT_END: 'event_id_shell_create-account_end',
-    EVENT_ID_TRACKING_OPT_IN: 'event_id_tracking_opt_in',
-    EVENT_ID_LOGIN_END: 'event_id_shell_login_end',
-    EVENT_ID_ERROR: 'event_id_shell_error'
+    EVENT_ID_CREATE_ACCOUNT_END: "event_id_shell_create-account_end",
+    EVENT_ID_TRACKING_OPT_IN: "event_id_tracking_opt_in",
+    EVENT_ID_LOGIN_END: "event_id_shell_login_end",
+    EVENT_ID_ERROR: "event_id_shell_error",
 };
