4eb1aca70a46640ecd44b119e6e61b7a75fff627
Matt Lockyer <mattdlockyer@gmail.com>
2020-10-07 08:42:10 -0700

    bulk of code moved to near-api-js #428

--- utils/2fa.js
+++ utils/2fa.js
@@ -8,53 +8,25 @@ const {
     },
 } = require('near-api-js');
 
+const getCode = async(method) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    const securityCode = await new Promise((res) => {
+        rl.question(chalk`{bold Enter security code sent to ${ method.kind.split('2fa-')[1] }: }`, (accountId) => res(accountId));
+    });
+    rl.close();
+    return securityCode;
+};
+
+const onResult = async(result) => {
+    console.log('Request confirmed with result:', result)
+};
+
 class Account2FA extends AccountMultisig {
     constructor(connection, accountId) {
-        super(connection, accountId);
-    }
-    // override for custom UX
-    async signAndSendTransaction(receiverId, actions) {
-
-        if (this.isDeleteAction(actions)) {
-            const { accountId } = this;
-            return await super.signAndSendTransaction(accountId, actions);
-        }
-
-        const method = await this.get2faMethod();
-        if (!method || !method.kind) {
-            throw new Error('no active 2fa method found');
-        }
-        
-        console.log('creating multisig request ...');
-        await super.signAndSendTransaction(receiverId, actions);
-        await this.sendRequestCode();
-
-        return await this.promptAndVerify(method);
-    }
-
-    async promptAndVerify(method) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        const securityCode = await new Promise((res) => {
-            rl.question(chalk`{bold Enter security code received via ${ method.kind.split('2fa-')[1] }: }`, (accountId) => res(accountId));
-        });
-        rl.close();
-        
-        console.log('verifying security code ...');
-        
-        try {
-            const { success, res: result } = await this.verifyRequestCode(securityCode);
-            if (!success || result === false) {
-                throw new Error('Request failed with error:', result);
-            }
-            console.log('Request confirmed with result:', typeof result === 'string' && result.length === 0 ? 'true' : result);
-            return result;
-        } catch (e) {
-            console.log('Invalid security code. Please try again.\n');
-            return await this.promptAndVerify(method);
-        }
+        super(connection, accountId, { getCode, onResult });
     }
 }
 
