5fc81ebed543ff5828672d14ef60fd4c76a6d04e
Willem Wyndham <willem@cs.umd.edu>
2020-11-18 10:37:50 -0500

    fix: eslint and findWasmFile

--- test/unit/find-wasm.test.js
+++ test/unit/find-wasm.test.js
@@ -5,56 +5,56 @@ const currentDir = path.dirname(__dirname);
 
 describe('find wasm', () => {
 
-  describe("successful", () => {
-    beforeAll(() => {
-        process.chdir(path.join(currentDir, "find-wasm32", "correct_find-wasm"));
+    describe('successful', () => {
+        beforeAll(() => {
+            process.chdir(path.join(currentDir, 'find-wasm32', 'correct_find-wasm'));
+        });
+
+        it('should find debug target', async () => {
+            expect(await findWasmFile('./out/main.wasm', 'debug')).toBe('rust/target/wasm32/debug/empty.wasm');
+        });
+
+        it('should find release target', async () => {
+            expect(await findWasmFile('./out/main.wasm', 'release')).toBe('rust/target/wasm32/release/empty.wasm');
+        });
     });
 
-    it('should find debug target', async () => {
-        expect(await findWasmFile('./out/main.wasm', 'debug')).toBe('rust/target/wasm32/debug/empty.wasm');
-    });
+    describe('too many files found', () => {
+        beforeAll(() => {
+            process.chdir(path.join(currentDir, 'find-wasm32', 'too_many_find-wasm'));
+        });
 
-    it('should find release target', async () => {
-        expect(await findWasmFile('./out/main.wasm', 'release')).toBe('rust/target/wasm32/release/empty.wasm');
-    });
-  });
-
-  describe("too many files found", () => {
-    beforeAll(() => {
-        process.chdir(path.join(currentDir, "find-wasm32", "too_many_find-wasm"));
-    });
-
-    it('should find too many debug targets', async () => {
-        await expect(findWasmFile('./out/main.wasm', 'debug')).rejects.toThrow(new Error(
-`Multiple Wasm files found:
+        it('should find too many debug targets', async () => {
+            await expect(findWasmFile('./out/main.wasm', 'debug')).rejects.toThrow(new Error(
+                `Multiple Wasm files found:
 \t• rust/target/wasm32/debug/empty.wasm
 \t• rust/target/wasm32/debug/empty1.wasm
 
-Please pass one with --wasmFile=file`))
-    });
+Please pass one with --wasmFile=file`));
+        });
 
-    it('should find too many release targets', async () => {
-        await expect(findWasmFile('./out/main.wasm', 'release')).rejects.toThrow(new Error(
-`Multiple Wasm files found:
+        it('should find too many release targets', async () => {
+            await expect(findWasmFile('./out/main.wasm', 'release')).rejects.toThrow(new Error(
+                `Multiple Wasm files found:
 \t• rust/target/wasm32/release/empty.wasm
 \t• rust/target/wasm32/release/empty1.wasm
 
-Please pass one with --wasmFile=file`))
-    });
-  });
-
-
-  describe("find out/main.wasm", () => {
-    beforeAll(() => {
-        process.chdir(path.join(currentDir, "find-wasm32", "has_out_main"));
+Please pass one with --wasmFile=file`));
+        });
     });
 
-    it('should find find the file', async () => {
-        expect(await findWasmFile('./out/main.wasm', 'debug')).toBe('./out/main.wasm');
-    });
+
+    describe('find out/main.wasm', () => {
+        beforeAll(() => {
+            process.chdir(path.join(currentDir, 'find-wasm32', 'has_out_main'));
+        });
+
+        it('should find find the file', async () => {
+            expect(await findWasmFile('./out/main.wasm', 'debug')).toBe('./out/main.wasm');
+        });
 
     
-  });
+    });
   
 
 });
--- utils/find-wasm.js
+++ utils/find-wasm.js
@@ -12,29 +12,29 @@ async function findWasm(target) {
 }
 
 async function findWasmFile(wasmFile, target) {
-  try {
-    return fileWasmFileSafe(wasmFile, target);
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        return findWasmFileSafe(wasmFile, target);
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 }
 
 async function findWasmFileSafe(wasmFile, target) {
-  if (fs.existsSync(wasmFile)) {
-      return wasmFile;
-  }
-  if (wasmFile === './out/main.wasm') {
-      let res = await findWasm(target);
-      if (res.length == 0) {
-          throw new Error(`Wasm file ${wasmFile} does not exist and none could be found for target ${target}.`);
-      }
-      if (res.length > 1) {
-          throw new Error(`Multiple Wasm files found:\n\t• ${res.join('\n\t• ')}\n\nPlease pass one with --wasmFile=file`);
-      }
-      return res[0];
-  }
-  throw new Error(`Wasm file ${wasmFile} does not exist.`);
+    if (fs.existsSync(wasmFile)) {
+        return wasmFile;
+    }
+    if (wasmFile === './out/main.wasm') {
+        let res = await findWasm(target);
+        if (res.length == 0) {
+            throw new Error(`Wasm file ${wasmFile} does not exist and none could be found for target ${target}.`);
+        }
+        if (res.length > 1) {
+            throw new Error(`Multiple Wasm files found:\n\t• ${res.join('\n\t• ')}\n\nPlease pass one with --wasmFile=file`);
+        }
+        return res[0];
+    }
+    throw new Error(`Wasm file ${wasmFile} does not exist.`);
 
 }
 
