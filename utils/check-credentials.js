0ff127916cc23d11b0fff9831afce9b8dfd1f91e
Josh <thisisjoshford@gmail.com>
2021-03-24 20:34:00 -0700

    add copy file to check credentials

--- utils/check-credentials.js
+++ utils/check-credentials.js
@@ -1,3 +1,6 @@
+const homedir = require('os').homedir();
+const credentials_dir = homedir + '/.near-credentials';
+
 module.exports = async function checkCredentials(options) {
   if(!(await options.keyStore.getKey(options.networkId, options.accountId))) {
     console.log(`Unable to find ${options.networkId} credentials for ${options.accountId}`);
@@ -6,11 +9,11 @@ module.exports = async function checkCredentials(options) {
         const answer = await askYesNoQuestion('Key found in deprecated \'default\' folder. Would you like to move key to \'testnet\' folder? (y/n): ', false);
         if(answer) {
             console.log('Moving files...');
-            // await fs.copyFile(`${credPath}/default/${option.accountId}`, `${credPath}/${options.networkId}/${options.accountId}`)
+            await fs.copyFile(`${credentials_dir}/default/${option.accountId}`, `${credentials_dir}/testnet/${options.accountId}`)
         } else {
-            console.log('Please relocate credentials found in \'default\' directory to \'testnet\'');
+            console.log('Please relocate credentials found in \'default\' directory to \'testnet\'.');
             return;
         }
     }
   }
-};
+}
