3fc72f86ac02ac1b777fe83fc0a20ab6ffe7138a
Josh <thisisjoshford@gmail.com>
2021-04-14 16:21:18 -0700

    check both testnet and default credential location

--- utils/check-credentials.js
+++ utils/check-credentials.js
@@ -5,23 +5,28 @@ module.exports = async function checkCredentials(
     networkId,
     keyStore
 ) {
-    if(networkId != 'testnet' || networkId != 'default') return;
+    if(networkId !== 'testnet' || networkId !== 'default') return;
     if (!(await keyStore.getKey(networkId, accountId))) {
         console.log(
             `Unable to find [ ${networkId} ] credentials for [ ${accountId} ]`
         );
-        const defaultKeyPair = await keyStore.getKey('default', accountId);
-        if (defaultKeyPair) {
+        const testnetKeys = await keyStore.getKey('testnet', accountId);
+        const defaultKeys = await keyStore.getKey('default', accountId);
+        if (defaultKeys && networkId === 'testnet') {
             const answer = await askYesNoQuestion(
                 'Key found in deprecated [ default ] folder. Would you like to copy your key to [ testnet ] folder? (y/n): ',
                 false
             );
             if (answer) {
                 console.log('Copying key and retrying transaction...');
-                await keyStore.setKey('testnet', accountId, defaultKeyPair);
+                await keyStore.setKey('testnet', accountId);
             } else {
                 throw 'Please relocate credentials found in [ default ] directory to [ testnet ] directory.';
             }
-        } else throw 'error';
+        } else if (testnetKeys && networkId === 'default') {
+            console.log(
+                'Key found in [ testnet ] folder but your command is configured to use deprecated [ default ] network. Please upgrade near-cli or change your project\'s config file. '
+            );
+        }
     }
 };
