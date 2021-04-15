290acd16c8fe7c583e376707d3a9bd900770732b
Josh <thisisjoshford@gmail.com>
2021-04-14 17:28:23 -0700

    style networkId and prompt near login

--- utils/check-credentials.js
+++ utils/check-credentials.js
@@ -9,13 +9,13 @@ module.exports = async function checkCredentials(
     if(networkId !== 'testnet' && networkId !== 'default') return;
     if (!(await keyStore.getKey(networkId, accountId))) {
         console.log(
-            chalk`{bold.white Unable to find [ ${networkId} ] credentials for [ ${accountId} ]}`
+            chalk`{bold.white Unable to find [ {bold.blue ${networkId}} ] credentials for [ {bold.blue ${accountId}} ]...}`
         );
         const testnetKeys = await keyStore.getKey('testnet', accountId);
         const defaultKeys = await keyStore.getKey('default', accountId);
         if (defaultKeys && networkId === 'testnet') {
             const answer = await askYesNoQuestion(
-                chalk`{bold.white Key found in {bold.yellow deprecated} [ default ] folder. Would you like to copy your key to [ testnet ] folder? (y/n)}`,
+                chalk`{bold.white Key found in {bold.yellow deprecated} [ {bold.blue default} ]  folder. Would you like to copy your key to [ {bold.blue testnet} ] folder? {bold.green (y/n)}}`,
                 false
             );
             if (answer) {
@@ -23,7 +23,7 @@ module.exports = async function checkCredentials(
                 await keyStore.setKey('testnet', accountId, defaultKeys);
             } else {
                 throw console.log(
-                    chalk`{bold.white Please relocate credentials found in [ default ] directory to [ testnet ] directory.}`
+                    chalk`{bold.white Please relocate credentials found in [ {bold.blue default} ] directory to [ {bold.blue testnet} ] directory using {bold.yellow near login}.}`
                 );
             }
         } else if (testnetKeys && networkId === 'default') {
