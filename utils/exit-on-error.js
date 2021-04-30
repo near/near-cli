cb3ae7cdae500155172e3e0c41d3cb7511de4af0
Daryl Collins <daryl@darylcollins.com>
2021-04-30 05:24:53 -0700

    Ensure NEAR_CLI_OPTIONS includes explicit data for `log-event` (#725)
    * Include only 'safe' data in `NEAR_CLI_OPTIONS` that is sent to `log-event.js` via environment variable
    
    * Re-add parsing of NEAR_CLI_OPTIONS from environment
    
    * Rename method for clarity and add a comment explaining why it's important

--- utils/exit-on-error.js
+++ utils/exit-on-error.js
@@ -1,6 +1,19 @@
 const eventtracking = require('./eventtracking');
 const inspectResponse = require('./inspect-response');
 
+/* `yargs` puts rich objects on the `options` object, which could include sensitive data (like a keystore!).
+ * This ensures we only put explicit, known non-private data that is relevant to tracking into the environment
+ * when we run `log-event.js`
+ */
+function getNonPrivateDataFromCmdlineOpts(options) {
+    return {
+        accountId: options.accountId,
+        networkId: options.networkId,
+        nodeUrl: options.nodeUrl,
+        walletUrl: options.walletUrl,
+    };
+}
+
 // This is a workaround to get Mixpanel to log a crash
 process.on('exit', () => {
     const crashEventProperties = {
@@ -12,7 +25,8 @@ process.on('exit', () => {
         detached: true,
         env: {
             NEAR_CLI_EVENT_ID: eventtracking.EVENT_ID_ERROR,
-            NEAR_CLI_EVENT_DATA: JSON.stringify(crashEventProperties)
+            NEAR_CLI_EVENT_DATA: JSON.stringify(crashEventProperties),
+            NEAR_CLI_OPTIONS: process.env.NEAR_CLI_OPTIONS
         }
     });
 });
@@ -22,13 +36,15 @@ module.exports = (promiseFn) => async (...args) => {
     process.env.NEAR_CLI_ERROR_LAST_COMMAND = command;
     process.env.NEAR_CLI_NETWORK_ID = require('../get-config')()['networkId'];
     const options = args[0];
-    const eventId =  `event_id_shell_${command}_start`;
+    const optionsAsStr = JSON.stringify(getNonPrivateDataFromCmdlineOpts(options));
+    const eventId = `event_id_shell_${command}_start`;
     require('child_process').fork(__dirname + '/log-event.js', ['node'], {
         silent: true,
         detached: true,
         env: {
             NEAR_CLI_EVENT_ID: eventId,
-            NEAR_CLI_EVENT_DATA: JSON.stringify({})
+            NEAR_CLI_EVENT_DATA: JSON.stringify({}),
+            NEAR_CLI_OPTIONS: optionsAsStr
         }
     });
     const promise = promiseFn.apply(null, args);
@@ -36,6 +52,7 @@ module.exports = (promiseFn) => async (...args) => {
         await promise;
     } catch (e) {
         process.env.NEAR_CLI_LAST_ERROR = e.message;
+        process.env.NEAR_CLI_OPTIONS = optionsAsStr;
         inspectResponse.prettyPrintError(e, options);
         process.exit(1);
     }
