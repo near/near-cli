ef6cbcdc26c7c26f1f65254f72183f3bf6e8e69a
Ryan Mehta <ryan.mehta@gmail.com>
2021-05-06 10:27:43 -0700

    Fix seed phrase so it works with masterAccount

--- middleware/seed-phrase.js
+++ middleware/seed-phrase.js
@@ -1,23 +1,25 @@
 const { parseSeedPhrase } = require('near-seed-phrase');
 const { utils: { KeyPair }, InMemorySigner } = require('near-api-js');
-const { InMemoryKeyStore } = require('near-api-js/lib/key_stores');
+const { InMemoryKeyStore, MergeKeyStore } = require('near-api-js/lib/key_stores');
 
 const implicitAccountId = require('../utils/implicit-accountid');
 
 // near ... --seedPhrase="phrase" --seedPath="m/44'/397'/0'"
 // near generate-key --seedPhrase="phrase"
-module.exports = async function useSeedPhrase({ seedPhrase, seedPath, publicKey, accountId, networkId }, yargs) {
+module.exports = async function useSeedPhrase({ seedPhrase, seedPath, keyStore, accountId, masterAccount, networkId }, yargs) {
     if (!seedPhrase) {
         return;
     }
     if (yargs.usingLedger) {
         throw new Error('Can not use both --useLedgerKey and --seedPhrase at the same time');
     }
-    const result = parseSeedPhrase(seedPhrase, seedPath);
-    publicKey = result.publicKey;
-    let keyStore = new InMemoryKeyStore();
-    accountId = accountId || implicitAccountId(publicKey);
-    await keyStore.setKey(networkId, accountId, KeyPair.fromString(result.secretKey));
-    let signer = new InMemorySigner(keyStore);
-    return { signer, publicKey, accountId };
+    const { publicKey, secretKey } = parseSeedPhrase(seedPhrase, seedPath);
+
+    const seedPhraseKeystore = new InMemoryKeyStore();
+    const seedPhraseAccountId = masterAccount ? masterAccount : accountId || implicitAccountId(publicKey);
+
+    await keyStore.setKey(networkId, seedPhraseAccountId, KeyPair.fromString(secretKey));
+    if(keyStore instanceof MergeKeyStore) keyStore.keyStores.push(seedPhraseKeystore);
+
+    return { keyStore, accountId };
 };
