da91a7c6302078ab2c3d65ddde2f0fe8e7e6f602
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-06-28 17:02:12 -0700

    changed directory to wasm file to  "build/release/contract. wasm"

--- test_environment.js
+++ test_environment.js
@@ -1,11 +1,11 @@
-const NodeEnvironment = require('jest-environment-node');
-const nearAPI = require('near-api-js');
-const fs = require('fs');
+const NodeEnvironment = require("jest-environment-node");
+const nearAPI = require("near-api-js");
+const fs = require("fs");
 
-const { PROJECT_KEY_DIR } = require('./middleware/key-store');
+const { PROJECT_KEY_DIR } = require("./middleware/key-store");
 
-const INITIAL_BALANCE = '500000000000000000000000000';
-const testAccountName = 'test.near';
+const INITIAL_BALANCE = "500000000000000000000000000";
+const testAccountName = "test.near";
 
 class LocalTestEnvironment extends NodeEnvironment {
     constructor(config) {
@@ -13,30 +13,43 @@ class LocalTestEnvironment extends NodeEnvironment {
     }
 
     async setup() {
-        this.global.nearlib = require('near-api-js');
-        this.global.nearAPI = require('near-api-js');
+        this.global.nearlib = require("near-api-js");
+        this.global.nearAPI = require("near-api-js");
         this.global.window = {};
-        let config = require('./get-config')();
+        let config = require("./get-config")();
         this.global.testSettings = this.global.nearConfig = config;
         const now = Date.now();
         // create random number with at least 7 digits
-        const randomNumber = Math.floor(Math.random() * (9999999 - 1000000) + 1000000);
+        const randomNumber = Math.floor(
+            Math.random() * (9999999 - 1000000) + 1000000
+        );
         config = Object.assign(config, {
-            contractName: 'test-account-' + now + '-' + randomNumber,
-            accountId: 'test-account-' + now + '-' + randomNumber
+            contractName: "test-account-" + now + "-" + randomNumber,
+            accountId: "test-account-" + now + "-" + randomNumber,
         });
-        const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(PROJECT_KEY_DIR);
+        const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
+            PROJECT_KEY_DIR
+        );
         config.deps = Object.assign(config.deps || {}, {
-            storage:  this.createFakeStorage(),
+            storage: this.createFakeStorage(),
             keyStore,
         });
         const near = await nearAPI.connect(config);
 
         const masterAccount = await near.account(testAccountName);
-        const randomKey = await nearAPI.KeyPair.fromRandom('ed25519');
-        const data = [...fs.readFileSync('./out/main.wasm')];
-        await config.deps.keyStore.setKey(config.networkId, config.contractName, randomKey);
-        await masterAccount.createAndDeployContract(config.contractName, randomKey.getPublicKey(), data, INITIAL_BALANCE);
+        const randomKey = await nearAPI.KeyPair.fromRandom("ed25519");
+        const data = [...fs.readFileSync("./build/release/contract.wasm")];
+        await config.deps.keyStore.setKey(
+            config.networkId,
+            config.contractName,
+            randomKey
+        );
+        await masterAccount.createAndDeployContract(
+            config.contractName,
+            randomKey.getPublicKey(),
+            data,
+            INITIAL_BALANCE
+        );
 
         await super.setup();
     }
@@ -52,18 +65,18 @@ class LocalTestEnvironment extends NodeEnvironment {
     createFakeStorage() {
         let store = {};
         return {
-            getItem: function(key) {
+            getItem: function (key) {
                 return store[key];
             },
-            setItem: function(key, value) {
+            setItem: function (key, value) {
                 store[key] = value.toString();
             },
-            clear: function() {
+            clear: function () {
                 store = {};
             },
-            removeItem: function(key) {
+            removeItem: function (key) {
                 delete store[key];
-            }
+            },
         };
     }
 }
