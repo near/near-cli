21b51a6db900769ecc504e71b281401034757ebc
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-10-19 01:07:04 +0300

    lint fix

--- commands/call.js
+++ commands/call.js
@@ -52,7 +52,7 @@ async function scheduleFunctionCall(options) {
     const near = await connect(options);
     const account = await near.account(options.accountId);
     const parsedArgs = options.base64 ? Buffer.from(options.args, 'base64') : JSON.parse(options.args || '{}');
-    console.log("Doing account.functionCall()");
+    console.log('Doing account.functionCall()');
     try {
         const functionCallResponse = await account.functionCall({
             contractId: options.contractName,
@@ -66,18 +66,18 @@ async function scheduleFunctionCall(options) {
         console.log(inspectResponse.formatResponse(result));
     } catch (error) {
         switch (JSON.stringify(error.kind)) {
-            case '{"ExecutionError":"Exceeded the prepaid gas."}': {
-                handleExceededThePrepaidGasError(error, options);
-                break;
-            }
-            default: {
-                console.log(error);
-            }
+        case '{"ExecutionError":"Exceeded the prepaid gas."}': {
+            handleExceededThePrepaidGasError(error, options);
+            break;
+        }
+        default: {
+            console.log(error);
+        }
         }
     }
 }
 
 function handleExceededThePrepaidGasError(error, options) {
     console.log(chalk.bold(`\nTransaction ${error.transaction_outcome.id} had ${options.gas} of attached gas but used ${error.transaction_outcome.outcome.gas_burnt} of gas`));
-    console.log("View this transaction in explorer:", chalk.blue(`https://explorer.${options.networkId}.near.org/transactions/${error.transaction_outcome.id}`));
+    console.log('View this transaction in explorer:', chalk.blue(`https://explorer.${options.networkId}.near.org/transactions/${error.transaction_outcome.id}`));
 }
