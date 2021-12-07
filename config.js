699e2b77ad3564d33d0d6db2b9abdd0a607e34af
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-12-07 18:26:27 +0200

    set-api-key command works

--- config.js
+++ config.js
@@ -3,69 +3,58 @@ const { getXApiKey } = require('./utils/x-api-key-settings.js');
 const CONTRACT_NAME = process.env.CONTRACT_NAME;
 
 function getConfig(env) {
+    let config;
     switch (env) {
-
     case 'production':
     case 'mainnet':
-        return {
+        config = {
             networkId: 'mainnet',
             nodeUrl: process.env.NEAR_CLI_RPC_SERVER_URL || 'https://rpc.mainnet.near.org',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             contractName: CONTRACT_NAME,
             walletUrl: 'https://wallet.near.org',
             helperUrl: 'https://helper.mainnet.near.org',
             helperAccount: 'near',
             explorerUrl: 'https://explorer.mainnet.near.org',
         };
+        break;
     case 'development':
     case 'testnet':
-        return {
+        config = {
             networkId: 'testnet',
             nodeUrl: process.env.NEAR_CLI_RPC_SERVER_URL || 'https://rpc.testnet.near.org',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             contractName: CONTRACT_NAME,
             walletUrl: 'https://wallet.testnet.near.org',
             helperUrl: 'https://helper.testnet.near.org',
             helperAccount: 'testnet',
             explorerUrl: 'https://explorer.testnet.near.org',
         };
+        break;
     case 'betanet':
-        return {
+        config = {
             networkId: 'betanet',
             nodeUrl: process.env.NEAR_CLI_RPC_SERVER_URL || 'https://rpc.betanet.near.org',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             contractName: CONTRACT_NAME,
             walletUrl: 'https://wallet.betanet.near.org',
             helperUrl: 'https://helper.betanet.near.org',
             helperAccount: 'betanet',
             explorerUrl: 'https://explorer.betanet.near.org',
         };
+        break;
     case 'guildnet':
-        return {
+        config = {
             networkId: 'guildnet',
             nodeUrl: process.env.NEAR_CLI_RPC_SERVER_URL || 'https://rpc.openshards.io',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             contractName: CONTRACT_NAME,
             walletUrl: 'https://wallet.openshards.io',
             helperUrl: 'https://helper.openshards.io',
             helperAccount: 'guildnet',
         };
+        break;
     case 'local':
     case 'localnet':
-        return {
+        config = {
             networkId: process.env.NEAR_CLI_LOCALNET_NETWORK_ID || 'local',
             nodeUrl: process.env.NEAR_NODE_URL || 'http://localhost:3030',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             keyPath: process.env.NEAR_CLI_LOCALNET_KEY_PATH || `${process.env.HOME}/.near/validator_key.json`,
             walletUrl: process.env.NEAR_WALLET_URL || 'http://localhost:4000/wallet',
             contractName: CONTRACT_NAME,
@@ -73,20 +62,23 @@ function getConfig(env) {
             helperAccount: process.env.NEAR_HELPER_ACCOUNT || 'node0',
             explorerUrl: process.env.NEAR_EXPLORER_URL || 'http://localhost:9001',
         };
+        break;
     case 'test':
     case 'ci':
-        return {
+        config = {
             networkId: 'shared-test',
             nodeUrl: process.env.NEAR_CLI_RPC_SERVER_URL || 'https://rpc.ci-testnet.near.org',
-            headers: { 'x-api-key': function() {
-                return getXApiKey(this.nodeUrl);
-            }},
             contractName: CONTRACT_NAME,
             masterAccount: 'test.near',
         };
+        break;
     default:
         throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
     }
+
+    // adding x-api-key for given RPC Server
+    config.headers = { 'x-api-key': getXApiKey(config.nodeUrl) };
+    return config;
 }
 
 module.exports = getConfig;
--- test/unit/utils/x-api-key-settings.test.js
+++ test/unit/utils/x-api-key-settings.test.js
@@ -1,11 +1,11 @@
-const { setXApiKey, getXApiKey, getAllXApiKeys } = require('../../../utils/x-api-key-settings.js');
+const { setXApiKey, getXApiKey } = require('../../../utils/x-api-key-settings.js');
 
-const RPC_SERVER_URL_1 = "https://test1.url.com";
-const RPC_SERVER_URL_2 = "https://test2.url.com";
-const RPC_SERVER_URL_3 = "https://test3.url.com";
-const RPC_SERVER_API_KEY_1 = "1814c8b3-d7e1-4145-ba75-d6fc9787b27d";
-const RPC_SERVER_API_KEY_2 = "2814c8b3-d7e1-4145-ba75-d6fc9787b27d";
-const RPC_SERVER_API_KEY_3 = "3814c8b3-d7e1-4145-ba75-d6fc9787b27d";
+const RPC_SERVER_URL_1 = 'https://test1.url.com';
+const RPC_SERVER_URL_2 = 'https://test2.url.com';
+const RPC_SERVER_URL_3 = 'https://test3.url.com';
+const RPC_SERVER_API_KEY_1 = '1814c8b3-d7e1-4145-ba75-d6fc9787b27d';
+const RPC_SERVER_API_KEY_2 = '2814c8b3-d7e1-4145-ba75-d6fc9787b27d';
+const RPC_SERVER_API_KEY_3 = '3814c8b3-d7e1-4145-ba75-d6fc9787b27d';
 
 describe('x-api-key-settings tests', () => {
     // set tests
@@ -42,18 +42,18 @@ describe('x-api-key-settings tests', () => {
     test('get API key for undefined throws error', async () => {
         expect(() => {
             getXApiKey(undefined);
-        }).toThrow(`Empty value provided. RPC Server URL: undefined`);
+        }).toThrow('Empty value provided. RPC Server URL: undefined');
     });
 
     test('get API key for empty string', async () => {
         expect(() => {
             getXApiKey('');
-        }).toThrow(`Empty value provided. RPC Server URL: `);
+        }).toThrow('Empty value provided. RPC Server URL: ');
     });
 
     test('get API key for null', async () => {
         expect(() => {
             getXApiKey(null);
-        }).toThrow(`Empty value provided. RPC Server URL: null`);
+        }).toThrow('Empty value provided. RPC Server URL: null');
     });
 });
