12ebc19b95807b653462a3453312128236521d62
Willem Wyndham <wyndham@cs.unc.edu>
2022-01-04 11:08:00 -0500

    fix: context interfaces type definition

--- context/index.d.ts
+++ context/index.d.ts
@@ -1,7 +1,7 @@
 import * as nearApi from "near-api-js";
 
 export declare interface Context {
-  nearAPI: nearApi,
+  nearAPI: typeof nearApi,
   near: nearApi.Near,
   account?: nearApi.Account,
   argv?: string[],
