63765d5880ae03cab136e378d9031ff7c68e908f
Phuong Nguyen <phuong@near.org>
2022-05-17 10:09:42 -0700

    Rearrange top level yargs for JS cli

--- commands/js.js
+++ commands/js.js
@@ -5,109 +5,113 @@ const connect = require('../utils/connect');
 const inspectResponse = require('../utils/inspect-response');
 const checkCredentials = require('../utils/check-credentials');
 
+const js_deploy = {
+    command: 'deploy [base64File] [initFunction] [initArgs] [initGas] [initDeposit]',
+    desc: 'Deploy our smart contract to the network',
+    builder: (yargs) => yargs
+        .option('base64File', {
+            desc: 'Path to base64 encoded contract file to deploy',
+            type: 'string',
+            required: true,
+        })
+        .option('gas', {
+            desc: 'Gas for deployment call',
+            type: 'number',
+            default: DEFAULT_FUNCTION_CALL_GAS,
+        })
+        .option('deposit', {
+            desc: 'Deposit to maintain the contract storage on the enclave',
+            type: 'string',
+            default: '0',
+        })
+        .option('depositYocto', {
+            desc: 'Deposit (in Yocto Near) to maintain the contract storage on the enclave',
+            type: 'string',
+            default: null,
+        })
+        .option('accountId', {
+            desc: 'Unique identifier for the account that will be used to sign this call',
+            type: 'string',
+            required: true,
+        })
+        .option('initFunction', {
+            desc: 'Initialization method',
+            type: 'string',
+        })
+        .option('jsvm', {
+            desc: 'JSVM enclave contract id',
+            type: 'string',
+            default: null,
+        }),
+    handler: exitOnError(deploy),
+};
+
+const js_remove = {
+    command: 'remove [accountId]',
+    desc: 'Remove a JSVM contract from the network',
+    builder: (yargs) => yargs
+        .option('accountId', {
+            desc: 'The id of the account that will be removed from the enclave',
+            type: 'string',
+            required: true,
+        })
+        .option('gas', {
+            desc: 'Gas used to remove the contract from the enclave',
+            type: 'number',
+            default: DEFAULT_FUNCTION_CALL_GAS,
+        })
+        .option('jsvm', {
+            desc: 'JSVM enclave contract id',
+            type: 'string',
+            default: null,
+        }),
+    handler: exitOnError(remove),
+};
+
+const js_call = {
+    command: 'call [contractId] [methodName] [args] [gas] [deposit]',
+    desc: 'Call a method on a contract',
+    builder: (yargs) => yargs
+        .option('accountId', {
+            desc: 'Unique identifier for the account that will be used to sign this call',
+            type: 'string',
+            required: true,
+        })
+        .option('args', {
+            desc: 'Arguments to the contract call, in JSON format by default (e.g. \'{"param_a": "value"}\')',
+            type: 'string',
+            default: '',
+        })
+        .option('gas', {
+            desc: 'Gas used to make a call into the contract from the enclave',
+            type: 'number',
+            default: DEFAULT_FUNCTION_CALL_GAS,
+        })
+        .option('deposit', {
+            desc: 'Deposit to maintain the contract storage on the enclave',
+            type: 'string',
+            default: '0',
+        })
+        .option('depositYocto', {
+            desc: 'Deposit (in Yocto Near) to maintain the contract storage on the enclave',
+            type: 'string',
+            default: null,
+        })
+        .option('jsvm', {
+            desc: 'JSVM enclave contract id',
+            type: 'string',
+            default: null,
+        }),
+    handler: exitOnError(call),
+};
 
 module.exports = {
     command: 'js <command> <command-options>',
     desc: 'Add an access key to given account',
     builder: (yargs) => yargs
-        .command({
-            command: 'deploy [base64File] [initFunction] [initArgs] [initGas] [initDeposit]',
-            desc: 'Deploy our smart contract to the network',
-            builder: (yargs) => yargs
-                .option('base64File', {
-                    desc: 'Path to base64 encoded contract file to deploy',
-                    type: 'string',
-                    required: true,
-                })
-                .option('gas', {
-                    desc: 'Gas for deployment call',
-                    type: 'number',
-                    default: DEFAULT_FUNCTION_CALL_GAS,
-                })
-                .option('deposit', {
-                    desc: 'Deposit to maintain the contract storage on the enclave',
-                    type: 'string',
-                    default: '0',
-                })
-                .option('depositYocto', {
-                    desc: 'Deposit (in Yocto Near) to maintain the contract storage on the enclave',
-                    type: 'string',
-                    default: null,
-                })
-                .option('accountId', {
-                    desc: 'Unique identifier for the account that will be used to sign this call',
-                    type: 'string',
-                    required: true,
-                })
-                .option('initFunction', {
-                    desc: 'Initialization method',
-                    type: 'string',
-                })
-                .option('jsvm', {
-                    desc: 'JSVM enclave contract id',
-                    type: 'string',
-                    default: null,
-                }),
-            handler: exitOnError(deploy),
-        })
-        .command({
-            command: 'remove [accountId]',
-            desc: 'Remove a JSVM contract from the network',
-            builder: (yargs) => yargs
-                .option('accountId', {
-                    desc: 'The id of the account that will be removed from the enclave',
-                    type: 'string',
-                    required: true,
-                })
-                .option('gas', {
-                    desc: 'Gas used to remove the contract from the enclave',
-                    type: 'number',
-                    default: DEFAULT_FUNCTION_CALL_GAS,
-                })
-                .option('jsvm', {
-                    desc: 'JSVM enclave contract id',
-                    type: 'string',
-                    default: null,
-                }),
-            handler: exitOnError(remove),
-        })
-        .command({
-            command: 'call [contractId] [methodName] [args] [gas] [deposit]',
-            desc: 'Call a method on a contract',
-            builder: (yargs) => yargs
-                .option('accountId', {
-                    desc: 'Unique identifier for the account that will be used to sign this call',
-                    type: 'string',
-                    required: true,
-                })
-                .option('args', {
-                    desc: 'Arguments to the contract call, in JSON format by default (e.g. \'{"param_a": "value"}\')',
-                    type: 'string',
-                    default: '',
-                })
-                .option('gas', {
-                    desc: 'Gas used to make a call into the contract from the enclave',
-                    type: 'number',
-                    default: DEFAULT_FUNCTION_CALL_GAS,
-                })
-                .option('deposit', {
-                    desc: 'Deposit to maintain the contract storage on the enclave',
-                    type: 'string',
-                    default: '0',
-                })
-                .option('depositYocto', {
-                    desc: 'Deposit (in Yocto Near) to maintain the contract storage on the enclave',
-                    type: 'string',
-                    default: null,
-                })
-                .option('jsvm', {
-                    desc: 'JSVM enclave contract id',
-                    type: 'string',
-                    default: null,
-                }),
-            handler: exitOnError(call),
-        })
-    ,
+        .command(js_deploy)
+        .command(js_remove)
+        .command(js_call),
 };
 
 // Encode JSVM related arguments into base64 for use in the enclave
