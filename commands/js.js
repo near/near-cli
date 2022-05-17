036a937aa57e122d3ed4658115caee25f8ee6a68
Phuong Nguyen <phuong@near.org>
2022-05-17 10:05:07 -0700

    Added jsvm_transact

--- commands/js.js
+++ commands/js.js
@@ -110,8 +110,20 @@ module.exports = {
     ,
 };
 
+// Encode JSVM related arguments into base64 for use in the enclave
+function base64_encode_args(contractId, functionName, args) {
+    let buf = Buffer.concat([
+        Buffer.from(contractId),
+        Buffer.from([0]),
+        Buffer.from(functionName),
+        Buffer.from([0]),
+        Buffer.from(args)]
+    );
+    return Buffer.from(buf.toString('base64'), 'base64');
+}
+
 function jsvm_contract_id(options) {
-    if (options.jsvm !== null) {
+    if (options.jsvm !== null && options.jsvm !== undefined) {
         return options.jsvm;
     }
 
@@ -126,23 +138,40 @@ function jsvm_contract_id(options) {
     throw Error(`Cannot find a default JSVM contract for network id ${option.networkId}`);
 }
 
-function base64_encode_args(contractId, functionName, args) {
-    let buf = Buffer.concat([
-        Buffer.from(contractId),
-        Buffer.from([0]),
-        Buffer.from(functionName),
-        Buffer.from([0]),
-        Buffer.from(args)]
-    );
-    return Buffer.from(buf.toString('base64'), 'base64');
+async function jsvm_transact(options, {methodName, args, gas, deposit}) {
+    const { accountId } = options;
+    const near = await connect(options);
+    const account = await near.account(accountId);
+    const jsvmId = jsvm_contract_id(options);
+
+    try {
+        const functionCallResponse = await account.functionCall({
+            contractId: jsvmId,
+            methodName: methodName,
+            args: args,
+            gas: gas,
+            attachedDeposit: deposit,
+        });
+        inspectResponse.prettyPrintResponse(functionCallResponse, options);
+
+        return providers.getTransactionLastResult(functionCallResponse);
+    } catch (error) {
+        switch (JSON.stringify(error.kind)) {
+            case '{"ExecutionError":"Exceeded the prepaid gas."}': {
+                handleExceededThePrepaidGasError(error, options);
+                break;
+            }
+            default: {
+                console.log(error);
+            }
+        }
+    }
 }
 
 async function deploy(options) {
     await checkCredentials(options.accountId, options.networkId, options.keyStore);
 
     const { accountId, base64File } = options;
-    const near = await connect(options);
-    const account = await near.account(accountId);
     const jsvmId = jsvm_contract_id(options);
     const deposit = options.depositYocto != null ? options.depositYocto : utils.format.parseNearAmount(options.deposit);
     const base64Contract = Buffer.from(readFileSync(base64File), 'base64');
@@ -150,92 +179,47 @@ async function deploy(options) {
     console.log(
         `Starting deployment. Account id: ${accountId}, JSVM: ${jsvmId}, file: ${base64File}`);
 
-    try {
-        const functionCallResponse = await account.functionCall({
-            contractId: jsvmId,
-            methodName: 'deploy_js_contract',
-            args: base64Contract,
-            gas: options.gas.toNumber(),
-            attachedDeposit: deposit,
-        });
-        inspectResponse.prettyPrintResponse(functionCallResponse, options);
-    } catch (error) {
-        switch (JSON.stringify(error.kind)) {
-            case '{"ExecutionError":"Exceeded the prepaid gas."}': {
-                handleExceededThePrepaidGasError(error, options);
-                break;
-            }
-            default: {
-                console.log(error);
-            }
-        }
-    }
+    await jsvm_transact(options, {
+        methodName: 'deploy_js_contract',
+        args: base64Contract,
+        gas: options.gas.toNumber(),
+        deposit,
+    });
 }
 
 async function remove(options) {
     await checkCredentials(options.accountId, options.networkId, options.keyStore);
 
     const { accountId } = options;
-    const near = await connect(options);
-    const account = await near.account(accountId);
     const jsvmId = jsvm_contract_id(options);
 
     console.log(
         `Removing contract from enclave. Account id: ${accountId}, JSVM: ${jsvmId}`);
 
-    try {
-        const functionCallResponse = await account.functionCall({
-            contractId: jsvmId,
-            methodName: 'remove_js_contract',
-            args: JSON.parse('{}'),
-            gas: options.gas.toNumber(),
-        });
-        inspectResponse.prettyPrintResponse(functionCallResponse, options);
-    } catch (error) {
-        switch (JSON.stringify(error.kind)) {
-            case '{"ExecutionError":"Exceeded the prepaid gas."}': {
-                handleExceededThePrepaidGasError(error, options);
-                break;
-            }
-            default: {
-                console.log(error);
-            }
-        }
-    }
+    await jsvm_transact(options, {
+        methodName: 'remove_js_contract',
+        args: JSON.parse('{}'),
+        gas: options.gas.toNumber(),
+        deposit: '0',
+    });
 }
 
 async function call(options) {
     await checkCredentials(options.accountId, options.networkId, options.keyStore);
 
-    const { accountId } = options;
-    const near = await connect(options);
-    const account = await near.account(accountId);
     const jsvmId = jsvm_contract_id(options);
     const deposit = options.depositYocto != null ? options.depositYocto : utils.format.parseNearAmount(options.deposit);
     const args = base64_encode_args(options.contractId, options.methodName, options.args);
-    // console.log(`args: ${args}`);
 
     console.log(`Scheduling a call in JSVM[${jsvmId}]: ${options.contractId}.${options.methodName}(${options.args || ''})` +
         (deposit && deposit != '0' ? ` with attached ${utils.format.formatNearAmount(deposit)} NEAR` : ''));
 
-    try {
-        const functionCallResponse = await account.functionCall({
-            contractId: jsvmId,
-            methodName: 'call_js_contract',
-            args: args,
-            gas: options.gas.toNumber(),
-            attachedDeposit: deposit,
-        });
-        inspectResponse.prettyPrintResponse(functionCallResponse, options);
-    } catch (error) {
-        switch (JSON.stringify(error.kind)) {
-            case '{"ExecutionError":"Exceeded the prepaid gas."}': {
-                handleExceededThePrepaidGasError(error, options);
-                break;
-            }
-            default: {
-                console.log(error);
-            }
-        }
-    }
+    const result = await jsvm_transact(options, {
+        methodName: 'call_js_contract',
+        args,
+        gas: options.gas.toNumber(),
+        deposit,
+    });
+
+    console.log(inspectResponse.formatResponse(result));
 }
