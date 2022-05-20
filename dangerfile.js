724b01fb804a8f0805e7af85ce4ff84037c4f064
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-05-20 18:37:36 +0300

    lint fix

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,8 @@
 const { danger, fail, warn } = require('danger');
 const { includes } = require('lodash');
 
-const hasPackageChanges = includes(danger.git.modified_files, "package.json")
-const hasLockfileChanges = includes(danger.git.modified_files, "yarn.lock")
+const hasPackageChanges = includes(danger.git.modified_files, 'package.json');
+const hasLockfileChanges = includes(danger.git.modified_files, 'yarn.lock');
 if (hasPackageChanges && !hasLockfileChanges) {
-    warn("There are package.json changes with no corresponding lockfile changes")
+    warn('There are package.json changes with no corresponding lockfile changes');
 }
