86401992c7e5780841f65dbf1255940b8ebf9346
Dennis <10233439+idea404@users.noreply.github.com>
2022-08-09 10:03:12 +0000

    update conditionals and stdout capturing

--- test/test_notify_optin.sh
+++ test/test_notify_optin.sh
@@ -8,41 +8,33 @@ echo Creating account
 ./bin/near create-account $testaccount > /dev/null
 
 echo Deploying contract to temporary accountId
-STDOUT=$(./bin/near dev-deploy ./test/res/guest_book.wasm -v 2>&1 >/dev/null | ./node_modules/.bin/strip-ansi)
-
-TEXT=$STDOUT
-EXPECTED='.+You may chose to opt in by running+'
-if [[ ! "$TEXT" =~ $EXPECTED ]]; then
+TEXT=$(./bin/near dev-deploy ./test/res/guest_book.wasm)
+EXPECTED='You may chose to opt in by running near track'
+if [[ ! "$TEXT" =~ .*"$EXPECTED".* ]]; then
     echo FAILURE Unexpected output from near call: "$TEXT"
     exit 1
 fi
 
 echo Invalid tracking parameter
-STDOUT=$(./bin/near track bleh > /dev/null)
-
-TEXT=$STDOUT
-EXPECTED='.+Invalid choice of+'
-if [[ ! "$TEXT" =~ $EXPECTED ]]; then
+TEXT=$(./bin/near track bleh)
+EXPECTED='Invalid choice of'
+if [[ ! "$TEXT" =~ .*"$EXPECTED".* ]]; then
     echo FAILURE Unexpected output from near call: "$TEXT"
     exit 1
 fi
 
 echo Activate tracking
-STDOUT=$(./bin/near track yes > /dev/null)
-
-TEXT=$STDOUT
-EXPECTED='.+Data tracking enabled+'
-if [[ ! "$TEXT" =~ $EXPECTED ]]; then
+TEXT=$(./bin/near track yes)
+EXPECTED='Data tracking enabled'
+if [[ ! "$TEXT" =~ .*"$EXPECTED".* ]]; then
     echo FAILURE Unexpected output from near call: "$TEXT"
     exit 1
 fi
 
 echo Disable tracking
-STDOUT=$(./bin/near track no > /dev/null)
-
-TEXT=$STDOUT
-EXPECTED='.+Data tracking disabled+'
-if [[ ! "$TEXT" =~ $EXPECTED ]]; then
+TEXT=$(./bin/near track no)
+EXPECTED='Data tracking disabled'
+if [[ ! "$TEXT" =~ .*"$EXPECTED".* ]]; then
     echo FAILURE Unexpected output from near call: "$TEXT"
     exit 1
 fi
