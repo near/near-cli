e1fdc036b5a879047203ac70acf4dfba55f01e23
gagdiez <gagdiez.c@gmail.com>
2024-02-29 18:31:27 +0100

    fix: override

--- commands/contract/deploy.js
+++ commands/contract/deploy.js
@@ -47,7 +47,7 @@ module.exports = {
     handler: deploy
 };
 
-const checkExistingContract = async function (prevCodeHash) {
+const askOverrideContract = async function (prevCodeHash) {
     if (prevCodeHash !== '11111111111111111111111111111111') {
         return await askYesNoQuestion(
             chalk`{bold.white This account already has a deployed contract [ {bold.blue ${prevCodeHash}} ]. Do you want to proceed? {bold.green (y/n) }}`,
@@ -65,8 +65,7 @@ async function deploy(options) {
     let prevState = await account.state();
     let prevCodeHash = prevState.code_hash;
 
-    const answeredYes = await checkExistingContract(prevCodeHash);
-    if (!options.force && !answeredYes) return;
+    if(!options.force && !(await askOverrideContract(prevCodeHash))) return;
 
     console.log(`Deploying contract ${options.wasmFile} in ${options.accountId}`);
 
