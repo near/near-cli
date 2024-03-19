67eb3173217726873ea053f3bb872ea83a66afcb
gagdiez <gagdiez.c@gmail.com>
2024-03-19 13:13:13 +0100

    fix: added message for Ledger users

--- utils/connect.js
+++ utils/connect.js
@@ -1,10 +1,12 @@
 const { connect: nearConnect } = require('near-api-js');
 const { getConfig } = require('../config');
 const { getPublicKeyForPath, signForPath } = require('./ledger');
+const chalk = require('chalk');
 
 module.exports = async function connect({ keyStore, ...options }) {
     // If using Ledger, override the signer so that it uses the Ledger device
     if (options.signWithLedger) { 
+        console.log(chalk`\nUsing Ledger with path {blue ${options.ledgerPath}}, change path with {blue --ledgerPath}\n`);
         options.signer = {
             getPublicKey: () => getPublicKeyForPath(options.ledgerPath),
             signMessage: (m) => signForPath(m, options.ledgerPath)
